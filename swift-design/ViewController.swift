3a3da52389f51600eee9403f3f77453166a67728
Berk Cohadar <berkcohadar1@gmail.com>
2021-09-29 10:41:02 -0400

    Project started.

---
+++ swift-design/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-design
+//
+//  Created by Berk Çohadar on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+
+}
+
